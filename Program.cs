cf15582e9ab901432576cb836b8b3730ad7a1d72
ADJ43 <adjse43@gmail.com>
2023-12-17 21:47:26 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_validation_Script
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00).
+            //A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok";
+            //otherwise, display "Invalid Time". If the user doesn't provide any values, consider it as invalid time.
+
+            Console.WriteLine("please enter time in HH:MM format");
+            string input = Console.ReadLine();
+
+            DateTime theTime;
+
+            if (DateTime.TryParse(input, out theTime))
+            {
+                Console.WriteLine("ok");
+            } else Console.WriteLine("invalid time");
+
+
+        }
+    }
+}
